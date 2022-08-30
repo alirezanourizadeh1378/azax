8fd8923f3c3ec21c369689d6e377a820abc9df7c
alirezanourizadeh1378 <112477649+alirezanourizadeh1378@users.noreply.github.com>
2022-08-30 14:31:42 +0430

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import time
+root=Tk()
+root.geometry("400x100")
+root.config(bg='blue')
+def update():
+    clock.config(text=time.strftime("%I:%M:%S"))
+    clock.after(1000,update)
+clock=Label(root,background='black',foreground='white',font=('arial',60,'bold'))
+clock.pack()
+update()
+root.mainloop()
